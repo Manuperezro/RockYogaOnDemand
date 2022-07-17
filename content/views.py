930572489b02aa80b6c14ce0ee46c7bf5ebbafb0
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-07-17 11:31:13 +0000

    Addded courses views and templates

--- content/views.py
+++ content/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Course
 
 # Create your views here.
+
+def course_list(request):
+    """ A view to show all courses, including sorting and searching queries """
+
+    courses = Course.objects.all()
+    print('courses', courses)
+    context = {
+        'courses': courses,
+    }
+    return render(request, 'content/course_list.html', context)
